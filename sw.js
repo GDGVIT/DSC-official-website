ee2578a403c999485cea8fc73b3929c1cf904121
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-07-12 21:02:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -143,15 +143,15 @@ self.addEventListener('activate', function(event) {
 //     })
 //   );
 // });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.open('mysite-dynamic').then(function(cache) {
+//       return cache.match(event.request).then(function (response) {
+//         return response || fetch(event.request).then(function(response) {
+//           cache.put(event.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
